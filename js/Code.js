e4f802112ecfef9c8f0d8127de2fd0815d7823a7
rafsywhj <70297474+rafsywhj@users.noreply.github.com>
2021-06-21 01:19:32 +0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    fill("white")
+    text("N C I N T O F U", 700, 150);
+    fill("lightblue")
+    text("Hint: Performs a particular task!!", 700,170)
+    fill("white")
+    text("E T A D A S B A", 400, 250);
+    fill("lightblue")
+    text("Hint: Stores All Information!!", 400,270)
+
+
+    
+
+}
